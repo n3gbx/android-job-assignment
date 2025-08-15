f395e745fff099853e771000ddf013a09d6c3760
Mikita <17089577+n3gbx@users.noreply.github.com>
2025-08-15 20:18:28 +0200

    Add MealDetailsScreen to nav graph

--- app/src/main/java/com/schibsted/nde/MainActivity.kt
+++ app/src/main/java/com/schibsted/nde/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.schibsted.nde.feature.mealdetails.MealDetailsScreen
 import com.schibsted.nde.feature.meals.MealsScreen
 import com.schibsted.nde.ui.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,11 +41,24 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController, startDestination = "meals") {
-        composable("meals") {
+        composable(
+            route = "meals"
+        ) {
             MealsScreen(
                 viewModel = hiltViewModel(),
                 onMealClick = { mealId ->
                     navController.navigate("mealDetails/$mealId")
+                }
+            )
+        }
+        composable(
+            route = "mealDetails/{mealId}",
+            arguments = listOf(navArgument("mealId") { type = NavType.StringType })
+        ) {
+            MealDetailsScreen(
+                viewModel = hiltViewModel(),
+                onBack = {
+                    navController.popBackStack()
                 }
             )
         }
