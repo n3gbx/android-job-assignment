1d7d4e35ed6476f165b83aea4d4aa4ddb3a5acc2
Mikita <17089577+n3gbx@users.noreply.github.com>
2025-08-15 19:25:44 +0200

    Update MealsRepository to cache data and refresh on demand

--- app/src/main/java/com/schibsted/nde/data/MealsRepository.kt
+++ app/src/main/java/com/schibsted/nde/data/MealsRepository.kt
@@ -1,13 +1,36 @@
 package com.schibsted.nde.data
 
 import com.schibsted.nde.api.BackendApi
+import com.schibsted.nde.data.common.ModelEntityMapper.mapToEntities
+import com.schibsted.nde.data.common.ModelEntityMapper.mapToModels
+import com.schibsted.nde.data.common.NetworkBoundResource
+import com.schibsted.nde.database.MealEntityDao
 import com.schibsted.nde.model.MealResponse
+import com.schibsted.nde.model.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MealsRepository @Inject constructor(
     private val backendApi: BackendApi,
+    private val mealDao: MealEntityDao,
+    private val networkBoundResource: NetworkBoundResource
 ) {
-    suspend fun getMeals(): List<MealResponse> = backendApi.getMeals().meals
+
+    fun getMeals(shouldRefresh: Boolean): Flow<Result<List<MealResponse>>> {
+        return networkBoundResource(
+            query = {
+                mealDao.getAll().map { it.mapToModels() }
+            },
+            fetch = {
+                backendApi.getMeals()
+            },
+            saveFetched = { mealsResponse ->
+                mealDao.insertAll(mealsResponse.mapToEntities())
+            },
+            shouldFetch = shouldRefresh
+        )
+    }
 }
