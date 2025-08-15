25ec3c5905d302115c35dc2e2c25e144d96d67dc
Mikita <17089577+n3gbx@users.noreply.github.com>
2025-08-15 19:03:09 +0200

    Implement NetworkBoundResource to fetch, cache and query data

---
+++ app/src/main/java/com/schibsted/nde/data/NetworkBoundResource.kt
@@ -0,0 +1,31 @@
+package com.schibsted.nde.data
+
+import com.schibsted.nde.model.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class NetworkBoundResource @Inject constructor() {
+
+    inline operator fun <QueryResult, FetchResult> invoke(
+        crossinline query: () -> Flow<QueryResult>,
+        crossinline fetch: suspend () -> FetchResult,
+        crossinline saveFetched: suspend (FetchResult) -> Unit,
+        shouldFetch: Boolean
+    ): Flow<Result<QueryResult>> = flow {
+        val flow: Flow<Result<QueryResult>> = runCatching {
+            if (shouldFetch) {
+                emit(Result.Loading())
+                saveFetched(fetch())
+            }
+            query().map { Result.Success(it) }
+        }.getOrElse { e ->
+            flowOf(Result.Error(e))
+        }
+
+        emitAll(flow)
+    }
+}
